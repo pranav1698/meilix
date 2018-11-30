fe47d1f8cad86e84b550d97e5cb9573dc0877c59
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-11-30 21:26:23 +0530

    test

--- test.sh
+++ test.sh
@@ -1,10 +1,1 @@
-newversion=$(date -u +%y.%m) # Should be derived from releasename $4 FIXME
- for oldversion in 17.08
- do
-  sed -i -e "s/${oldversion}/${newversion}/g" image/isolinux/*.hlp image/isolinux/f1.txt
- done
- newdate=$(date -u +%Y%m%d)
- for olddate in 2010clear113 20100928
- do
-  sed -i -e "s/${olddate}/${newdate}/g" image/isolinux/*.hlp image/isolinux/f1.txt
- done
+aaa
