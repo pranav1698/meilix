f1ca8cbc9dcc6154a7894f376e0bf23f77eb3385
Pranav Kulshrestha <hulkcodexl16@gmail.com>
2018-12-19 23:44:36 +0530

    Ab to aaja

--- scripts/chroot_reconst.sh
+++ scripts/chroot_reconst.sh
@@ -80,6 +80,9 @@ cat /usr/share/applications/lxqt-about.desktop
 sed -i '$ a NoDisplay=true' /usr/share/applications/lxqt-about.desktop
 cat /usr/share/applications/lxqt-about.desktop
 
+# Meilix default settings
+dpkg -i --force-overwrite meilix-default-settings_1.0_all.deb
+
 # Clean up the chroot before
 perl -i -nle 'print unless /^Package: language-(pack|support)/ .. /^$/;' /var/lib/apt/extended_states
 apt-get -qq clean
