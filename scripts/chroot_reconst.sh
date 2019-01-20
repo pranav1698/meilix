e16f2579b90f08cd4adebf15dfcf360d7c9229a5
pranav1698 <hulkcodexl16@gmail.com>
2019-01-20 14:55:51 +0530

    Addded plymouth

--- scripts/chroot_reconst.sh
+++ scripts/chroot_reconst.sh
@@ -39,8 +39,8 @@ apt-get -qq -y --purge install ubuntu-standard casper lupin-casper \
   laptop-detect os-prober linux-generic
 
 # Install base packages
-#apt-get -qq -y install xorg lightdm  plymouth-label ttf-ubuntu-font-family
-apt-get -qq -y install xorg xinit sddm
+#apt-get -qq -y install xorg lightdm
+apt-get -qq -y install xorg xinit sddm plymouth-label ttf-ubuntu-font-family
 # Install LXQT components
 apt-get -qq -y install lxqt openbox 
 apt-get -f install
