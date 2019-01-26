df049c7556963db2274691a816777aec3bafe975
pranav1698 <hulkcodexl16@gmail.com>
2019-01-26 17:50:50 +0530

    Added loopback test

--- build.sh
+++ build.sh
@@ -163,7 +163,7 @@ sed -i 's/Kubuntu/meilix/' image/.disk/info
 
 ls image/boot/grub
 [ -f image/boot/grub/grub.cfg ] && cat image/boot/grub/grub.cfg
-sed -i 's/Lubuntu/Meilix/' image/boot/grub/grub.cfg && cat image/boot/grub/loopback.cfg
+sed -i 's/Lubuntu/Meilix/' image/boot/grub/loopback.cfg && cat image/boot/grub/loopback.cfg
 
 sudo rm image/isolinux/back.jpg
 cp -Rf $TRAVIS_BUILD_DIR/back.jpg image/isolinux
