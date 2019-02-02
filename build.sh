7ab5acee127651005ced56e64db4078efb0e361f
pranav1698 <hulkcodexl16@gmail.com>
2019-02-03 02:25:49 +0530

    added tests

--- build.sh
+++ build.sh
@@ -26,7 +26,10 @@ datafiles="image-${arch}.tar.lzma sources.${release}.list"
 # Necessary development tool packages to be installed on build host
 devtools="debootstrap genisoimage p7zip-full squashfs-tools ubuntu-dev-tools"
 
-echo $TRAVIS_SCRIPT
+if [ "$TRAVIS_SCRIPT" == ""]
+then
+  echo "pranav"
+fi
 
 # Make sure we have the data files we need
 for i in $datafiles
