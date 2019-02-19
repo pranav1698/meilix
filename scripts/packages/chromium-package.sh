601c2fdfb73dd6fba6e88563b354b55d69367a6b
pranav1698 <hulkcodexl16@gmail.com>
2019-02-19 22:59:20 +0530

    Test

--- scripts/packages/chromium-package.sh
+++ scripts/packages/chromium-package.sh
@@ -1,2 +1,5 @@
 #!/bin/bash
-sudo apt-get install -q -y chromium-browser
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add - 
+sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google.list'
+sudo apt-get -qq update
+sudo apt-get -qq -y install google-chrome-stable
