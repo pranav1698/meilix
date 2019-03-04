b2acf0c49f39dd944c3f1db82ff360d1ab3ebbef
pranav1698 <hulkcodexl16@gmail.com>
2019-03-04 20:25:50 +0530

    test

--- scripts/packages/chromium-package.sh
+++ scripts/packages/chromium-package.sh
@@ -1,5 +1,9 @@
 #!/bin/bash
+# Installing chromium 32 bit or 64 bit browser 
+if [ "$arch" == "i386" ];
+then 
+wget security.ubuntu.com/ubuntu/pool/universe/c/chromium-browser/chromium-browser_71.0.3578.98-0ubuntu0.16.04.1_i386.deb -O chromium-browser_i386.deb
+sudo dpkg -i  chromium-browser_i386.deb
+else 
 sudo apt-get -qq -y install chromium-browser
-
-# Installing pepper flash plugin for chromium web-browser
-sudo apt-get -qq -y install pepperflashplugin-nonfree
+fi
