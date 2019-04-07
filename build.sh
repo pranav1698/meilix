dca7af7743379c12b6a842651685516505e01826
pranav1698 <hulkcodexl16@gmail.com>
2019-04-07 17:20:03 +0530

    dark-theme test

--- build.sh
+++ build.sh
@@ -78,7 +78,7 @@ chmod +x ./scripts/debuild.sh
 #Fetch the packages from meilix-artwork
 wget -q https://github.com/fossasia/meilix-artwork/raw/deb/plymouth-theme-meilix-logo_1.0-2_all.deb -O plymouth-theme-meilix-logo_1.0-2_all.deb
 wget -q https://github.com/fossasia/meilix-artwork/raw/deb/plymouth-theme-meilix-text_1.0-2_all.deb -O plymouth-theme-meilix-text_1.0-2_all.deb
-wget -q https://github.com/fossasia/meilix-artwork/raw/deb/meilix-default-theme_1.0-2_all.deb -O meilix-default-theme_1.0-2_all.deb
+wget -q https://github.com/pranav1698/meilix-artwork/blob/theme-test/meilix-default-dark-theme_1.0-2_all.deb -O meilix-default-theme_1.0-2_all.deb
 wget -q https://github.com/fossasia/meilix-systemlock/raw/master/systemlock_0.1-1_all.deb -O systemlock_0.1-1_all.deb
 
 # Create and populate the chroot using debootstrap
