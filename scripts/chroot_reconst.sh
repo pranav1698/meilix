28951fc701f2c3f4cacf9f642152cfe61fe9b009
pranav1698 <hulkcodexl16@gmail.com>
2019-04-17 01:34:31 +0530

    Test

--- scripts/chroot_reconst.sh
+++ scripts/chroot_reconst.sh
@@ -45,7 +45,9 @@ apt-get -qq -y --purge install ubuntu-standard casper lupin-casper \
 #apt-get -qq -y install xorg lightdm
 apt-get -qq -y install xorg xinit sddm
 # Install LXQT components
-apt-get -qq -y install lxqt openbox
+apt-get install openbox pcmanfm-qt lxqt-admin lxqt-common lxqt-config lxqt-globalkeys \
+ lxqt-notificationd lxqt-panel lxqt-policykit lxqt-powermanagement lxqt-qtplugin \
+  lxqt-runner lxqt-session lxqt-sudo
 apt-get -f -qq install
 update-alternatives --install /usr/bin/x-session-manager x-session-manager /usr/bin/startlxqt 140
 # ugly hack
